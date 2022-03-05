81097878d06fbaa619e65a4795ca56b87cf56ccc
Erick-Marinho <erickgaves@gmail.com>
2022-03-05 18:59:58 -0300

    :wrench: Instalando e configurando Tailwind

---
+++ components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Header: NextPage = () => {
+	return (
+		<>
+			<header>
+				<div>Logo</div>
+				<div>Search</div>
+				<div>Minha Conta</div>
+				<div>Carrinho</div>
+			</header>
+		</>
+	);
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Header from '../components/Header/Header';
 
 const Home: NextPage = () => {
 	return (
@@ -7,9 +8,7 @@ const Home: NextPage = () => {
 			<Head>
 				<title>Forever Baby</title>
 			</Head>
-			<div>
-				<h1>Forever Baby</h1>
-			</div>
+			<Header />
 		</>
 	);
 };
