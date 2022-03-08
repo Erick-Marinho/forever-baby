695c6bcdf42bc00a5a0f73fa531e28a895922410
Erick-Marinho <erickgaves@gmail.com>
2022-03-08 17:30:33 -0300

    :sparkles: Implementando cards

--- components/Cards/Cards.tsx
+++ components/Cards/Cards.tsx
@@ -1,12 +1,48 @@
 import type { NextPage } from 'next';
+import Card from '../Card/Card';
+import logo from '../../assets/logoForeverBaby.png';
 
+interface Elements {
+	img: StaticImageData;
+	text: string;
+	price: string;
+}
+
+const elements: Elements | Elements[] = [
+	{
+		img: logo,
+		text: 'Lorem ipsum lorem ipsum',
+		price: 'R$ 99,99',
+	},
+	{
+		img: logo,
+		text: 'Lorem ipsum lorem ipsum',
+		price: 'R$ 99,99',
+	},
+	{
+		img: logo,
+		text: 'Lorem ipsum lorem ipsum',
+		price: 'R$ 99,99',
+	},
+	{
+		img: logo,
+		text: 'Lorem ipsum lorem ipsum',
+		price: 'R$ 99,99',
+	},
+];
 const Cards: NextPage = () => {
 	return (
 		<>
-			<div className='w-64 h-96 border-solid border-2'></div>
-			<div className='w-64 h-96 border-solid border-2'></div>
-			<div className='w-64 h-96 border-solid border-2'></div>
-			<div className='w-64 h-96 border-solid border-2'></div>
+			{elements.map((item, index: number): any => {
+				return (
+					<Card
+						key={index}
+						text={item.text}
+						price={item.price}
+						img={item.img}
+					/>
+				);
+			})}
 		</>
 	);
 };
--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,9 +1,35 @@
 import type { NextPage } from 'next';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Carousel: NextPage = () => {
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	};
+
 	return (
 		<>
-			<div className='w-full h-96 bg-[#FFFFFF]'></div>
+			<div className='w-full h-96 bg-[#FFFFFF]'>
+				<Slider {...settings}>
+					<div>
+						<h3>1</h3>
+					</div>
+					<div>
+						<h3>2</h3>
+					</div>
+					<div>
+						<h3>3</h3>
+					</div>
+					<div>
+						<h3>4</h3>
+					</div>
+				</Slider>
+			</div>
 		</>
 	);
 };
